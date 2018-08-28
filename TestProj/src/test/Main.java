8a387a1b86a2563b86a390b13892a92f34a6cee2
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-08-28 10:46:08 -0400

    Renamed items, added some structures

--- TestProj/src/test/Main.java
+++ TestProj/src/test/Main.java
@@ -4,22 +4,37 @@ import java.io.*;
 
 public class Main {
 	
-	static String MainString;
+	static String InputString;
+	static String[] InputStringArray;
 	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	
 	
 	public static void main(String[] args) {
+		System.out.println("CONSOLE TESTING ENVIRONMENT\n"
+				+ "Year: 2018; By: JASON ZHANG");
 		
-		
-		
+		for(;;) {
+			try {
+				getInputToInputString(">>>");
+				InputStringArray = InputString.split(" ");
+				
+				switch(InputString) {
+					
+				}
+				}
+			catch(Exception e) {
+				System.out.print("ERROR, Exception thrown\n");
+			}
+		}
 	}
 	
 	/*
-	 * NEW LINE WILL BE PRINTED
+	 * NEW LINE WILL BE PRINTED AFTERWARDS
 	 * TEXT MUST NOT HAVE TERMINATING NEWLINE
 	 */
-	public static void getInputToMainString(String text) throws IOException {
-		System.out.print("\n" + text);
-		MainString = reader.readLine();
+	public static void getInputToInputString(String text) throws IOException {
+		System.out.print(text);
+		InputString = reader.readLine();
+		System.out.print("\n");
 	}
 }
