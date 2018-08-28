73edfe48ff3c1ea6733f4e5c47e8ecb4de500c3c
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-08-28 10:37:11 -0400

    Added getInputToMainString(String text) throws IOException function

--- TestProj/src/test/Main.java
+++ TestProj/src/test/Main.java
@@ -1,11 +1,25 @@
 package test;
 
-public class Main {
+import java.io.*;
 
+public class Main {
+	
+	static String MainString;
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	
+	
 	public static void main(String[] args) {
-		String s = "Hello, world!";
-		System.out.println(s);
+		
+		
 		
 	}
-
+	
+	/*
+	 * NEW LINE WILL BE PRINTED
+	 * TEXT MUST NOT HAVE TERMINATING NEWLINE
+	 */
+	public static void getInputToMainString(String text) throws IOException {
+		System.out.print("\n" + text);
+		MainString = reader.readLine();
+	}
 }
