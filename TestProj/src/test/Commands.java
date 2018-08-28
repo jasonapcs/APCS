ca3937a5a7869ef5fb303b54e67ae07926e0893b
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-08-28 11:19:08 -0400

    Implemented Echo

--- TestProj/src/test/Commands.java
+++ TestProj/src/test/Commands.java
@@ -12,7 +12,23 @@ public class Commands {
 		}
 		catch(Exception e) {
 			System.out.print("Error: Syntax\n"
-					+ "\'exit\' command syntax is : exit <ExitCode>\n");
+					+ "\'exit\' command syntax is: exit <ExitCode>\n");
 		}
-	}	
+	}
+	
+	/*
+	 * This function executes the command 'echo'
+	 * It also handles any exceptions that 
+	 */
+	public static void echoCommand() {
+		try {
+			Main.InputStringArray = Main.InputString.split(" ", 2);
+			System.out.print(Main.InputStringArray[1]);
+			System.out.print("\n");
+		}
+		catch(Exception e) {
+			System.out.print("Error: Syntax\n"
+					+ "\'echo\' command syntax is: echo <String>\n");
+		}
+	}
 }
