748e1e10a4bd85f1eac9898633eaff8926466de7
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-09-04 10:27:23 -0400

    Moved exit into class + implementation

---
+++ TestProj/src/test/exitCommand.java
@@ -0,0 +1,23 @@
+package test;
+
+public class exitCommand extends Command {
+	
+	/*
+	 * This function execute the command 'exit'
+	 * It also handles exceptions from execution code
+	 */
+	public void execute(String Input) {
+		int temp;
+		try {
+			String InputArray[] = Input.split(" ");
+			temp = Integer.parseInt(InputArray[1]);
+			System.exit(temp);
+		}
+		catch(Exception e) {
+			System.out.print("Error: Syntax\n"
+					+ "\'exit\' command syntax is: exit <ExitCode>\n");
+		}
+	}
+	
+	public String commandID = "exit";
+}
