18ee1bdb7b168c27b87cfab6ec1abe491ea362a9
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-09-04 10:27:35 -0400

    Moved echo into class and added implementation

---
+++ TestProj/src/test/echoCommand.java
@@ -0,0 +1,22 @@
+package test;
+
+public class echoCommand extends Command {
+	
+	/*
+	 * This function executes the command 'echo'
+	 * It also handles any exceptions that could occur
+	 */
+	public void execute(String Input) {
+		try {
+			String InputArray[] = Input.split(" ", 2);
+			System.out.print(InputArray[1]);
+			System.out.print("\n");
+		}
+		catch(Exception e) {
+			System.out.print("Error: Syntax\n"
+					+ "\'echo\' command syntax is: echo <String>\n");
+		}
+	}
+	
+	public String commandID = "echo";
+}
